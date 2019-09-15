b128d6ca28dd2e38977e4ca9fe87ee9c57e502ef
younhoso <thdbsgh3443@naver.com>
2019-09-15 18:26:07 +0900

    190915 커밋

---
+++ ImagesSlice-version2/5.imageslides/5-1/main.js
@@ -0,0 +1,15 @@
+
+var container = $('.slide_wrap'),
+    slideShow = container.find('.slide_show'),
+    slideImg = slideShow.find('.slide_img'),
+    slideImgDiv = slideImg.find('>div'),
+    slideBtn = container.find('.slide_btn'),
+    slideDot = container.find('.slide_dot');
+
+var slideImgSize = slideImgDiv.length;
+var currentIndex = 0;
+var dot = '';
+
+$(function(){
+
+});
